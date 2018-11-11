0d8e6274e815b99ce8989b70fe1c42e8017edfd3
wallerprogramm <dominik.sommer@uzh.ch>
2018-11-11 23:37:48 +0100

    Renaming

--- src/master.cpp
+++ src/master.cpp
@@ -43,29 +43,29 @@ void Master::phase3() {
             ) {
         std::vector<std::pair<Address, int> > cost_R;
         std::vector<std::pair<Address, int> > cost_S;
-        for (auto &tup : it->second)
+        for (auto &tuple : it->second)
         {
-            if (std::get<2>(tup) == R)
+            if (std::get<2>(tuple) == R)
             {
-                cost_R.emplace_back(std::get<0>(tup), std::get<1>(tup)/*multiply width R*/);
+                cost_R.emplace_back(std::get<0>(tuple), std::get<1>(tuple)/*multiply width R*/);
             }
             else
             {
-                cost_S.emplace_back(std::get<0>(tup), std::get<1>(tup)/*multiply width S*/);
+                cost_S.emplace_back(std::get<0>(tuple), std::get<1>(tuple)/*multiply width S*/);
             }
         }
         if (broadcast(cost_R, cost_S) < broadcast(cost_S, cost_R))
         {
-            for (auto &tup : it->second)
+            for (auto &tuple : it->second)
             {
-                if (std::get<2>(tup) == R)
+                if (std::get<2>(tuple) == R)
                 {
                     for (auto &t : it->second)
                     {
                         if (std::get<2>(t) == S)
                         {
                             std::tuple<int, int, relation_type> key_node_service(it->first, std::get<0>(t).id(), S);
-                            TCP_Client<std::tuple<int, int, relation_type>> client(key_node_service, std::get<0>(tup).ip(), std::get<0>(tup).service());
+                            TCP_Client<std::tuple<int, int, relation_type>> client(key_node_service, std::get<0>(tuple).ip(), std::get<0>(tuple).service());
                         }
                     }
                 }
@@ -73,16 +73,16 @@ void Master::phase3() {
         }
         else
         {
-            for (auto &tup : it->second)
+            for (auto &tuple : it->second)
             {
-                if (std::get<2>(tup) == S)
+                if (std::get<2>(tuple) == S)
                 {
                     for (auto &t : it->second)
                     {
                         if (std::get<2>(t) == R)
                         {
                             std::tuple<int, int, relation_type> key_node_service(it->first, std::get<0>(t).id(), R);
-                            TCP_Client<std::tuple<int, int, relation_type>> client(key_node_service, std::get<0>(tup).ip(), std::get<0>(tup).service());
+                            TCP_Client<std::tuple<int, int, relation_type>> client(key_node_service, std::get<0>(tuple).ip(), std::get<0>(tuple).service());
                         }
                     }
                 }
