1314ab447a9d04072e02ea9dec24a9baaadfb5db
Alphonse Mariya <alphonse.mariya@hotmail.com>
2018-12-03 11:06:00 +0100

    Generate random relation directly using sadDB_gen

--- generator_src/main.cpp
+++ generator_src/main.cpp
@@ -1,9 +1,11 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <random>
 #include <string>
 #include <vector>
 
+#include <boost/lexical_cast.hpp>
 #include "boost/archive/text_oarchive.hpp"
 #include "boost/serialization/vector.hpp"
 #include "boost/tuple/tuple.hpp"
@@ -17,7 +19,8 @@ namespace
 
 void print_help()
 {
-	std::cout << "usage: " << program_name << " [port]" << std::endl;
+	std::cout << "usage: " << program_name << " [-i <file>] [-o <file>]"
+		<< " [-p <payload>] [-t <tuples>] [-r <min> <max>]" << std::endl;
 }
 
 template<typename C>
@@ -31,22 +34,93 @@ int main(int argc, char** argv)
 {
 	std::vector<std::string> args(argv, argv + argc);
 
-	if (argc < 3)
+	// first argument contains program name
+	program_name = args.front();
+	args.erase(args.begin());
+
+	if (argc < 1)
 	{
 		print_help();
 		return 1;
 	}
 
-	// first argument contains program name
-	program_name = args.front();
-	args.erase(args.begin());
+	// options
+	std::string input;
+	std::string output;
+
+	// options for random generator
+	unsigned int n_payload = 0;
+	unsigned int n_tuples = 0;
+	int min = 0;
+	int max = 0;
+
+	for (int i = 0; i < args.size(); ++i)
+	{
+		if ("-i" == args[i])
+		{
+			input = args[++i];
+		}
+
+		else if ("-o" == args[i])
+		{
+			output = args[++i];
+		}
+
+		else if ("-p" == args[i])
+		{
+			n_payload = boost::lexical_cast<unsigned int>(args[++i]);
+		}
+
+		else if ("-t" == args[i])
+		{
+			n_tuples = boost::lexical_cast<unsigned int>(args[++i]);
+		}
+
+		else if ("-r" == args[i])
+		{
+			min = boost::lexical_cast<int>(args[++i]);
+			max = boost::lexical_cast<int>(args[++i]);
+		}
+
+		else if ("-h" == args[i])
+		{
+			print_help();
+			return 0;
+		}
+
+		else
+		{
+			print_help();
+			return 1;
+		}
+	}
 
 	// relation
 	Relation relation;
 
-	// read records
-	std::ifstream ifs(args[0]);
-	ifs >> relation;
+	// if input file given
+	if (!input.empty())
+	{
+		// read records
+		std::ifstream ifs(input);
+		ifs >> relation;
+	}
+	else
+	{
+		std::default_random_engine generator;
+		std::uniform_int_distribution<int> distribution(min, max);
+
+		std::string payload;
+		for (int i = 0; i < n_payload; ++i)
+			payload.push_back('.');
+
+		relation.reserve(n_tuples);
+		for (int i = 0; i < n_tuples; ++i)
+		{
+			relation.insert(Record(distribution(generator),
+						std::vector { payload }));
+		}
+	}
 
 	// serialize relation
 	std::ostringstream archive_stream;
@@ -54,7 +128,7 @@ int main(int argc, char** argv)
 	archive << relation;
 
 	// save serialized relation
-	std::ofstream ofs(args[1]);
+	std::ofstream ofs(output);
 	ofs << archive_stream.str();
 
 	return 0;
